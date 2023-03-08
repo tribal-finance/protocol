b0aa527f384942cdae838d1807cd70a12d5ee1ef
Seva Rybakov <jumpincode@gmail.com>
2023-03-08 16:44:48 +0800

    move usdc token distributions to a helper

--- contracts/ITestUSDC.sol
+++ contracts/ITestUSDC.sol
@@ -1,6 +1,6 @@
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IUSDC is IERC20 {
+interface ITestUSDC is IERC20 {
     function owner() external view returns (address);
 
     function updateMasterMinter(address _newMasterMinter) external;
--- test/MainnetForkUSDC.ts
+++ test/MainnetForkUSDC.ts
@@ -2,42 +2,31 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import setupUSDC, { USDC_PRECISION } from "./helpers/usdc";
 
 const { parseUnits } = ethers.utils;
 
-const USDC_ADDRESS_6 = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-const USDC_WHALE_ADDRESS = "0xF977814e90dA44bFA03b6295A0616a897441aceC"; // Binance. Has 3kkk USDC;
-
 describe("MainnetForkUSDC", function () {
-  async function testSetup() {
-    const usdc = await ethers.getContractAt("IUSDC", USDC_ADDRESS_6);
-    const [signer] = await ethers.getSigners();
-    const signerAddress = await signer.getAddress();
-    const value = ethers.utils.hexlify(ethers.utils.zeroPad(signerAddress, 32));
+  it("gives 1,000,000 USDC to first signer", async function () {
+    const { usdc, signerAddresses } = await loadFixture(setupUSDC);
 
-    // set contract owner to first signer
-    await ethers.provider.send("hardhat_setStorageAt", [
-      USDC_ADDRESS_6,
-      "0x0",
-      value,
-    ]);
-
-    // set master minter to first signer
-    await usdc.updateMasterMinter(await signer.getAddress());
-
-    await usdc.configureMinter(
-      await signer.getAddress(),
-      parseUnits("1", 9 + 6)
+    expect(await usdc.balanceOf(signerAddresses[0])).to.eq(
+      parseUnits("1000000", USDC_PRECISION)
     );
+  });
 
-    await usdc.mint(await signer.getAddress(), parseUnits("1", 6 + 6));
+  it("gives 1,000,000 USDC to second signer", async function () {
+    const { usdc, signerAddresses } = await loadFixture(setupUSDC);
 
-    return { usdc, signer };
-  }
+    expect(await usdc.balanceOf(signerAddresses[1])).to.eq(
+      parseUnits("1", 6 + USDC_PRECISION)
+    );
+  });
 
-  it("works", async function () {
-    const { usdc, signer } = await loadFixture(testSetup);
-    expect(await usdc.balanceOf(await signer.getAddress())).to.eq(
+  it("gives 1,000,000 USDC to third signer too", async function () {
+    const { usdc, signerAddresses } = await loadFixture(setupUSDC);
+
+    expect(await usdc.balanceOf(signerAddresses[3])).to.eq(
       parseUnits("1", 6 + 6)
     );
   });
