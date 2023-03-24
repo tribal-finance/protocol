6e156374f2051cd53c55f828cad9779252729e26
Seva Rybakov <jumpincode@gmail.com>
2023-03-25 01:26:26 +0800

    refactor the deployment tests

--- lib/pool_deployments.ts
+++ lib/pool_deployments.ts
@@ -14,274 +14,384 @@ import { ethers } from "hardhat";
 import type { BigNumberish, Signer } from "ethers";
 import type {
   ERC20Upgradeable,
+  ILendingPool,
   LendingPool,
   PoolFactory,
 } from "../typechain-types";
 import type { IUSDC } from "../typechain-types/contracts/IUSDC";
+import { pool } from "../typechain-types/contracts";
+import { USDC_ADDRESS_6 } from "../test/helpers/usdc";
+import { USDC, WAD } from "../test/helpers/conversion";
 
 const { parseUnits } = ethers.utils;
 
 const WAIT_CONFIRMATIONS = 1;
 
-export type CommonInput = {
-  deployer: Signer;
-  lender1: Signer;
-  lender2: Signer;
-  borrower: Signer;
-  usdcContract: ERC20Upgradeable;
-  poolFactoryContract: PoolFactory;
+export const DEFAULT_LENDING_POOL_PARAMS = {
+  name: "Test Pool",
+  token: "TST",
+  stableCoinContractAddress: USDC_ADDRESS_6,
+  minFundingCapacity: USDC(10000),
+  maxFundingCapacity: USDC(12000),
+  fundingPeriodSeconds: 24 * 60 * 60,
+  lendingTermSeconds: 12 * 30 * 24 * 60 * 60,
+  borrowerTotalInterestRateWad: WAD(0.15),
+  collateralRatioWad: WAD(0.2),
+  defaultPenalty: 0,
+  penaltyRateWad: WAD(0.01),
+  tranchesCount: 1,
+  trancheAPYsWads: [WAD(0.1)],
+  trancheBoostedAPYsWads: [WAD(0.1)],
+  trancheCoveragesWads: [WAD(1)],
 };
+export const DEFAULT_MULTITRANCHE_FUNDING_SPLIT = [WAD(0.2), WAD(0.8)];
 
-export async function _deployPool(
-  name: string,
-  poolDuration: BigNumberish,
-  amount: BigNumberish,
-  {
-    deployer,
-    lender1,
-    lender2,
-    borrower,
-    usdcContract,
-    poolFactoryContract,
-  }: CommonInput
-): Promise<string> {
-  console.log("* deploying pool");
-  await poolFactoryContract
+export async function deployFactoryAndImplementations(
+  deployer: Signer,
+  borrower: Signer,
+  lenders: Array<Signer>
+): Promise<PoolFactory> {
+  const LendingPool = await ethers.getContractFactory("LendingPool");
+  const poolImplementation = await LendingPool.connect(deployer).deploy();
+  await poolImplementation.deployed();
+
+  const FirstLossCapitalVault = await ethers.getContractFactory(
+    "FirstLossCapitalVault"
+  );
+  const flcImplementation = await FirstLossCapitalVault.connect(
+    deployer
+  ).deploy();
+  await flcImplementation.deployed();
+
+  const TrancheVault = await ethers.getContractFactory("TrancheVault");
+  const trancheVaultImplementation = await TrancheVault.connect(
+    deployer
+  ).deploy();
+  await trancheVaultImplementation.deployed();
+
+  const PoolFactory = await ethers.getContractFactory("PoolFactory");
+  const poolFactory = await PoolFactory.connect(deployer).deploy();
+  await poolFactory.deployed();
+
+  await poolFactory.connect(deployer).initialize();
+  await poolFactory
     .connect(deployer)
-    .deployUnitranchePool(
-      name + "2",
-      "TST",
-      usdcContract.address,
-      amount,
-      poolDuration,
-      parseUnits("0.1", 18),
-      parseUnits("0.2", 18),
-      await borrower.getAddress()
-    );
+    .setPoolImplementation(poolImplementation.address);
+  await poolFactory
+    .connect(deployer)
+    .setTrancheVaultImplementation(trancheVaultImplementation.address);
+  await poolFactory
+    .connect(deployer)
+    .setFirstLossCapitalVaultImplementation(flcImplementation.address);
 
-  const { poolAddress } = await poolFactoryContract.lastDeployedPoolRecord();
-  console.log(
-    "* pool address: https://goerli.etherscan.io/address/" +
-      ethers.utils.hexlify(poolAddress)
-  );
-  return poolAddress;
+  return poolFactory;
 }
 
-async function _lender1Deposit800(
-  poolContract: LendingPool,
-  params: CommonInput
+export async function deployUnitranchePool(
+  poolFactory: PoolFactory,
+  deployer: Signer,
+  borrower: Signer,
+  lenders: Array<Signer>,
+  poolInitParamsOverrides: Partial<ILendingPool.LendingPoolParamsStruct> = {},
+  fundingSplitWads = [WAD(1)]
 ) {
-  const { usdcContract, lender1 } = params;
-  console.log("* set allowance for lender 1");
-  await (
-    await usdcContract
-      .connect(lender1)
-      .increaseAllowance(poolContract.address, parseUnits("800", 6))
-  ).wait(WAIT_CONFIRMATIONS);
-
-  console.log("* deposit $800 from lender 1");
-  await (
-    await poolContract
-      .connect(lender1)
-      .deposit(parseUnits("800", 6), await lender1.getAddress())
-  ).wait(WAIT_CONFIRMATIONS);
-}
-
-async function _lender2Deposit200(
-  poolContract: LendingPool,
-  params: CommonInput
-) {
-  const { usdcContract, lender2 } = params;
-  console.log("* set allowance for lender 2");
-  await (
-    await usdcContract
-      .connect(lender2)
-      .increaseAllowance(poolContract.address, parseUnits("200", 6))
-  ).wait(WAIT_CONFIRMATIONS);
-  console.log("* deposit $200 from lender 2");
-  await (
-    await poolContract
-      .connect(lender2)
-      .deposit(parseUnits("200", 6), await lender2.getAddress())
-  ).wait(WAIT_CONFIRMATIONS + 5);
-}
-
-async function _borrowerBorrows(
-  poolContract: LendingPool,
-  params: CommonInput
-) {
-  const { usdcContract, borrower } = params;
-  console.log("* borrow as borrower");
-  await (
-    await poolContract.connect(borrower).borrow()
-  ).wait(WAIT_CONFIRMATIONS);
-}
-
-async function _borrowerPays50Interest(
-  poolContract: LendingPool,
-  params: CommonInput
-) {
-  const { usdcContract, borrower } = params;
-  console.log("* approve spend $50 for borrower");
-  await (
-    await usdcContract
-      .connect(borrower)
-      .increaseAllowance(poolContract.address, parseUnits("50", 6))
-  ).wait(WAIT_CONFIRMATIONS);
-  console.log("* borrower pays 50 interest");
-  await (
-    await poolContract
-      .connect(borrower)
-      .borrowerPayInterest(parseUnits("50", 6))
-  ).wait(WAIT_CONFIRMATIONS);
-}
-
-async function _borrowerPays150Interest(
-  poolContract: LendingPool,
-  params: CommonInput
-) {
-  const { usdcContract, borrower } = params;
-  console.log("* approve spend $150 for borrower");
-  await (
-    await usdcContract
-      .connect(borrower)
-      .increaseAllowance(poolContract.address, parseUnits("150", 6))
-  ).wait(WAIT_CONFIRMATIONS);
-  console.log("* borrower pays $150 interest");
-  await (
-    await poolContract
-      .connect(borrower)
-      .borrowerPayInterest(parseUnits("150", 6))
-  ).wait(WAIT_CONFIRMATIONS);
-}
-
-export async function deployOpenPool(params: CommonInput): Promise<string> {
-  const address = await _deployPool(
-    "FreshPool",
-    365 * 24 * 60 * 60,
-    parseUnits("1000", 6),
-    params
+  const lendingPoolParams: ILendingPool.LendingPoolParamsStruct = Object.assign(
+    DEFAULT_LENDING_POOL_PARAMS,
+    {
+      borrowerAddress: await borrower.getAddress(),
+    },
+    poolInitParamsOverrides
   );
 
-  return address;
-}
+  await poolFactory.deployPool(lendingPoolParams, [WAD(1)]);
 
-export async function deployHalveFoundedPool(
-  params: CommonInput
-): Promise<string> {
-  const { usdcContract, lender1 } = params;
-  const poolAddress = await _deployPool(
-    "HalveFounded",
-    365 * 24 * 60 * 60,
-    parseUnits("1000", 6),
-    params
+  const lastDeployedPoolRecord = await poolFactory.lastDeployedPoolRecord();
+
+  const unitranchePool = await ethers.getContractAt(
+    "LendingPool",
+    lastDeployedPoolRecord.poolAddress
   );
 
-  const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
-  await _lender1Deposit800(poolContract, params);
-  return poolAddress;
-}
-
-export async function deployFoundedPool(params: CommonInput): Promise<string> {
-  const { usdcContract, lender1, lender2 } = params;
-  const poolAddress = await _deployPool(
-    "FoundedPool",
-    365 * 24 * 60 * 60,
-    parseUnits("1000", 6),
-    params
+  const firstLossCapitalVault = await ethers.getContractAt(
+    "FirstLossCapitalVault",
+    lastDeployedPoolRecord.firstLossCapitalVaultAddress
   );
 
-  const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
-  await _lender1Deposit800(poolContract, params);
-  await _lender2Deposit200(poolContract, params);
-
-  return poolAddress;
-}
-
-export async function deploy5daysFoundedPool(
-  params: CommonInput
-): Promise<string> {
-  const { usdcContract, lender1, lender2 } = params;
-  const poolAddress = await _deployPool(
-    "5 days 10k pool",
-    6 * 24 * 60 * 60,
-    parseUnits("10000", 6),
-    params
+  const firstTrancheVault = await ethers.getContractAt(
+    "TrancheVault",
+    lastDeployedPoolRecord.firstTrancheVaultAddress
   );
 
-  const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
-
-  console.log("* set allowance for lender 1");
-  await (
-    await usdcContract
-      .connect(lender1)
-      .increaseAllowance(poolContract.address, parseUnits("10000", 6))
-  ).wait(WAIT_CONFIRMATIONS);
-
-  console.log("* deposit $10,000 from lender 1");
-  await (
-    await poolContract
-      .connect(lender1)
-      .deposit(parseUnits("10000", 6), await lender1.getAddress())
-  ).wait(WAIT_CONFIRMATIONS);
-
-  return poolAddress;
+  return {
+    deployer,
+    borrower,
+    lenders,
+    poolFactory,
+    unitranchePool,
+    firstLossCapitalVault,
+    firstTrancheVault,
+  };
 }
 
-export async function deployBorrowedPool(params: CommonInput): Promise<string> {
-  const { usdcContract, lender1, lender2, borrower } = params;
-  const poolAddress = await _deployPool(
-    "BorrowedPool",
-    365 * 24 * 60 * 60,
-    parseUnits("1000", 6),
-    params
-  );
+// export type CommonInput = {
+//   deployer: Signer;
+//   lender1: Signer;
+//   lender2: Signer;
+//   borrower: Signer;
+//   usdcContract: ERC20Upgradeable;
+//   poolFactoryContract: PoolFactory;
+// };
 
-  const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
-  await _lender1Deposit800(poolContract, params);
-  await _lender2Deposit200(poolContract, params);
-  await _borrowerBorrows(poolContract, params);
+// export async function _deployPool(
+//   name: string,
+//   poolDuration: BigNumberish,
+//   amount: BigNumberish,
+//   {
+//     deployer,
+//     lender1,
+//     lender2,
+//     borrower,
+//     usdcContract,
+//     poolFactoryContract,
+//   }: CommonInput
+// ): Promise<string> {
+//   console.log("* deploying pool");
+//   await poolFactoryContract
+//     .connect(deployer)
+//     .deployUnitranchePool(
+//       name + "2",
+//       "TST",
+//       usdcContract.address,
+//       amount,
+//       poolDuration,
+//       parseUnits("0.1", 18),
+//       parseUnits("0.2", 18),
+//       await borrower.getAddress()
+//     );
 
-  return poolAddress;
-}
+//   const { poolAddress } = await poolFactoryContract.lastDeployedPoolRecord();
+//   console.log(
+//     "* pool address: https://goerli.etherscan.io/address/" +
+//       ethers.utils.hexlify(poolAddress)
+//   );
+//   return poolAddress;
+// }
 
-export async function deployBorrowedHalveInterestRepaidPool(
-  params: CommonInput
-): Promise<string> {
-  const { usdcContract, lender1, lender2, borrower } = params;
-  const poolAddress = await _deployPool(
-    "BorrowedHalveInterstRepaidPool",
-    365 * 24 * 60 * 60,
-    parseUnits("1000", 6),
-    params
-  );
+// async function _lender1Deposit800(
+//   poolContract: LendingPool,
+//   params: CommonInput
+// ) {
+//   const { usdcContract, lender1 } = params;
+//   console.log("* set allowance for lender 1");
+//   await (
+//     await usdcContract
+//       .connect(lender1)
+//       .increaseAllowance(poolContract.address, parseUnits("800", 6))
+//   ).wait(WAIT_CONFIRMATIONS);
 
-  const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
-  await _lender1Deposit800(poolContract, params);
-  await _lender2Deposit200(poolContract, params);
-  await _borrowerBorrows(poolContract, params);
-  await _borrowerPays50Interest(poolContract, params);
+//   console.log("* deposit $800 from lender 1");
+//   await (
+//     await poolContract
+//       .connect(lender1)
+//       .deposit(parseUnits("800", 6), await lender1.getAddress())
+//   ).wait(WAIT_CONFIRMATIONS);
+// }
 
-  return poolAddress;
-}
+// async function _lender2Deposit200(
+//   poolContract: LendingPool,
+//   params: CommonInput
+// ) {
+//   const { usdcContract, lender2 } = params;
+//   console.log("* set allowance for lender 2");
+//   await (
+//     await usdcContract
+//       .connect(lender2)
+//       .increaseAllowance(poolContract.address, parseUnits("200", 6))
+//   ).wait(WAIT_CONFIRMATIONS);
+//   console.log("* deposit $200 from lender 2");
+//   await (
+//     await poolContract
+//       .connect(lender2)
+//       .deposit(parseUnits("200", 6), await lender2.getAddress())
+//   ).wait(WAIT_CONFIRMATIONS + 5);
+// }
 
-export async function deployBorrowedFullInterestRepaidPool(
-  params: CommonInput
-): Promise<string> {
-  const { usdcContract, lender1, lender2, borrower } = params;
-  const poolAddress = await _deployPool(
-    "BorrowedHalveInterstRepaidPool",
-    365 * 24 * 60 * 60,
-    parseUnits("1000", 6),
-    params
-  );
+// async function _borrowerBorrows(
+//   poolContract: LendingPool,
+//   params: CommonInput
+// ) {
+//   const { usdcContract, borrower } = params;
+//   console.log("* borrow as borrower");
+//   await (
+//     await poolContract.connect(borrower).borrow()
+//   ).wait(WAIT_CONFIRMATIONS);
+// }
 
-  const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
-  await _lender1Deposit800(poolContract, params);
-  await _lender2Deposit200(poolContract, params);
-  await _borrowerBorrows(poolContract, params);
-  await _borrowerPays50Interest(poolContract, params);
-  await _borrowerPays150Interest(poolContract, params);
+// async function _borrowerPays50Interest(
+//   poolContract: LendingPool,
+//   params: CommonInput
+// ) {
+//   const { usdcContract, borrower } = params;
+//   console.log("* approve spend $50 for borrower");
+//   await (
+//     await usdcContract
+//       .connect(borrower)
+//       .increaseAllowance(poolContract.address, parseUnits("50", 6))
+//   ).wait(WAIT_CONFIRMATIONS);
+//   console.log("* borrower pays 50 interest");
+//   await (
+//     await poolContract
+//       .connect(borrower)
+//       .borrowerPayInterest(parseUnits("50", 6))
+//   ).wait(WAIT_CONFIRMATIONS);
+// }
 
-  return poolAddress;
-}
+// async function _borrowerPays150Interest(
+//   poolContract: LendingPool,
+//   params: CommonInput
+// ) {
+//   const { usdcContract, borrower } = params;
+//   console.log("* approve spend $150 for borrower");
+//   await (
+//     await usdcContract
+//       .connect(borrower)
+//       .increaseAllowance(poolContract.address, parseUnits("150", 6))
+//   ).wait(WAIT_CONFIRMATIONS);
+//   console.log("* borrower pays $150 interest");
+//   await (
+//     await poolContract
+//       .connect(borrower)
+//       .borrowerPayInterest(parseUnits("150", 6))
+//   ).wait(WAIT_CONFIRMATIONS);
+// }
+
+// export async function deployOpenPool(params: CommonInput): Promise<string> {
+//   const address = await _deployPool(
+//     "FreshPool",
+//     365 * 24 * 60 * 60,
+//     parseUnits("1000", 6),
+//     params
+//   );
+
+//   return address;
+// }
+
+// export async function deployHalveFoundedPool(
+//   params: CommonInput
+// ): Promise<string> {
+//   const { usdcContract, lender1 } = params;
+//   const poolAddress = await _deployPool(
+//     "HalveFounded",
+//     365 * 24 * 60 * 60,
+//     parseUnits("1000", 6),
+//     params
+//   );
+
+//   const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
+//   await _lender1Deposit800(poolContract, params);
+//   return poolAddress;
+// }
+
+// export async function deployFoundedPool(params: CommonInput): Promise<string> {
+//   const { usdcContract, lender1, lender2 } = params;
+//   const poolAddress = await _deployPool(
+//     "FoundedPool",
+//     365 * 24 * 60 * 60,
+//     parseUnits("1000", 6),
+//     params
+//   );
+
+//   const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
+//   await _lender1Deposit800(poolContract, params);
+//   await _lender2Deposit200(poolContract, params);
+
+//   return poolAddress;
+// }
+
+// export async function deploy5daysFoundedPool(
+//   params: CommonInput
+// ): Promise<string> {
+//   const { usdcContract, lender1, lender2 } = params;
+//   const poolAddress = await _deployPool(
+//     "5 days 10k pool",
+//     6 * 24 * 60 * 60,
+//     parseUnits("10000", 6),
+//     params
+//   );
+
+//   const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
+
+//   console.log("* set allowance for lender 1");
+//   await (
+//     await usdcContract
+//       .connect(lender1)
+//       .increaseAllowance(poolContract.address, parseUnits("10000", 6))
+//   ).wait(WAIT_CONFIRMATIONS);
+
+//   console.log("* deposit $10,000 from lender 1");
+//   await (
+//     await poolContract
+//       .connect(lender1)
+//       .deposit(parseUnits("10000", 6), await lender1.getAddress())
+//   ).wait(WAIT_CONFIRMATIONS);
+
+//   return poolAddress;
+// }
+
+// export async function deployBorrowedPool(params: CommonInput): Promise<string> {
+//   const { usdcContract, lender1, lender2, borrower } = params;
+//   const poolAddress = await _deployPool(
+//     "BorrowedPool",
+//     365 * 24 * 60 * 60,
+//     parseUnits("1000", 6),
+//     params
+//   );
+
+//   const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
+//   await _lender1Deposit800(poolContract, params);
+//   await _lender2Deposit200(poolContract, params);
+//   await _borrowerBorrows(poolContract, params);
+
+//   return poolAddress;
+// }
+
+// export async function deployBorrowedHalveInterestRepaidPool(
+//   params: CommonInput
+// ): Promise<string> {
+//   const { usdcContract, lender1, lender2, borrower } = params;
+//   const poolAddress = await _deployPool(
+//     "BorrowedHalveInterstRepaidPool",
+//     365 * 24 * 60 * 60,
+//     parseUnits("1000", 6),
+//     params
+//   );
+
+//   const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
+//   await _lender1Deposit800(poolContract, params);
+//   await _lender2Deposit200(poolContract, params);
+//   await _borrowerBorrows(poolContract, params);
+//   await _borrowerPays50Interest(poolContract, params);
+
+//   return poolAddress;
+// }
+
+// export async function deployBorrowedFullInterestRepaidPool(
+//   params: CommonInput
+// ): Promise<string> {
+//   const { usdcContract, lender1, lender2, borrower } = params;
+//   const poolAddress = await _deployPool(
+//     "BorrowedHalveInterstRepaidPool",
+//     365 * 24 * 60 * 60,
+//     parseUnits("1000", 6),
+//     params
+//   );
+
+//   const poolContract = await ethers.getContractAt("LendingPool", poolAddress);
+//   await _lender1Deposit800(poolContract, params);
+//   await _lender2Deposit200(poolContract, params);
+//   await _borrowerBorrows(poolContract, params);
+//   await _borrowerPays50Interest(poolContract, params);
+//   await _borrowerPays150Interest(poolContract, params);
+
+//   return poolAddress;
+// }
--- test/factory/PoolFactory.ts
+++ test/factory/PoolFactory.ts
@@ -2,86 +2,29 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { BigNumberish } from "ethers";
 import setupUSDC, { USDC_PRECISION, USDC_ADDRESS_6 } from "../helpers/usdc";
-import { FirstLossCapitalVault__factory } from "../../typechain-types";
+import {
+  FirstLossCapitalVault__factory,
+  PoolFactory,
+} from "../../typechain-types";
 import { USDC, WAD } from "../helpers/conversion";
+import {
+  deployFactoryAndImplementations,
+  deployUnitranchePool,
+} from "../../lib/pool_deployments";
 
 describe("PoolFactory", function () {
   async function fixture() {
-    const [deployer, lender1, lender2, borrower] = await ethers.getSigners();
+    const [deployer, lender1, lender2, lender3, borrower] =
+      await ethers.getSigners();
+    const lenders = [lender1, lender2, lender3];
 
-    const LendingPool = await ethers.getContractFactory("LendingPool");
-    const poolImplementation = await LendingPool.deploy();
-    await poolImplementation.deployed();
-
-    const FirstLossCapitalVault = await ethers.getContractFactory(
-      "FirstLossCapitalVault"
-    );
-    const flcImplementation = await FirstLossCapitalVault.deploy();
-    await flcImplementation.deployed();
-
-    const TrancheVault = await ethers.getContractFactory("TrancheVault");
-    const trancheVaultImplementation = await TrancheVault.deploy();
-    await trancheVaultImplementation.deployed();
-
-    const PoolFactory = await ethers.getContractFactory("PoolFactory");
-    const poolFactory = await PoolFactory.deploy();
-    await poolFactory.deployed();
-
-    await poolFactory.initialize();
-    await poolFactory.setPoolImplementation(poolImplementation.address);
-    await poolFactory.setTrancheVaultImplementation(
-      trancheVaultImplementation.address
-    );
-    await poolFactory.setFirstLossCapitalVaultImplementation(
-      flcImplementation.address
-    );
-
-    await poolFactory.deployPool(
-      {
-        name: "Test Pool",
-        token: "TST",
-        stableCoinContractAddress: USDC_ADDRESS_6,
-        minFundingCapacity: USDC(10000),
-        maxFundingCapacity: USDC(12000),
-        fundingPeriodSeconds: 24 * 60 * 60,
-        lendingTermSeconds: 12 * 30 * 24 * 60 * 60,
-        borrowerAddress: borrower.address,
-        borrowerTotalInterestRateWad: WAD(0.15),
-        collateralRatioWad: WAD(0.2),
-        defaultPenalty: 0,
-        penaltyRateWad: WAD(0.01),
-        tranchesCount: 1,
-        trancheAPYsWads: [WAD(0.1)],
-        trancheBoostedAPYsWads: [WAD(0.1)],
-        trancheCoveragesWads: [WAD(1)],
-      },
-      [WAD(1)]
-    );
-
-    const lastDeployedPoolRecord = await poolFactory.lastDeployedPoolRecord();
-
-    const unitranchePool = await ethers.getContractAt(
-      "LendingPool",
-      lastDeployedPoolRecord.poolAddress
-    );
-
-    const firstLossCapitalVault = await ethers.getContractAt(
-      "FirstLossCapitalVault",
-      lastDeployedPoolRecord.firstLossCapitalVaultAddress
-    );
-
-    const firstTrancheVault = await ethers.getContractAt(
-      "TrancheVault",
-      lastDeployedPoolRecord.firstTrancheVaultAddress
-    );
-
-    return {
+    const poolFactory: PoolFactory = await deployFactoryAndImplementations(
       deployer,
-      poolFactory,
-      unitranchePool,
-      firstLossCapitalVault,
-      firstTrancheVault,
-    };
+      borrower,
+      lenders
+    );
+
+    return await deployUnitranchePool(poolFactory, deployer, borrower, lenders);
   }
 
   describe("When unitranche pool is deployed", async () => {
