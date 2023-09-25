fd53ff261e563c522103fff4448241b9d56c5467
teddy-tribal <teddy.clapp@tribal.credit>
2023-09-25 13:12:09 -0700

    dev ops

--- tasks/live-state-testing/encodings/duotranche/encode-defaulted.sh
+++ tasks/live-state-testing/encodings/duotranche/encode-defaulted.sh
@@ -5,21 +5,21 @@ export BORROWER_ADDRESS="0x8DfA5E23c8bd7911ea7A31b180b1572B5858300B"
 export PLATFORM_TOKEN_ADDRESS="0x0f81CdC8c06CF924B829E9FDA1f616fA716322F2"
 
 npx hardhat encode-pool-initializer \
-  --name "v1.0.3 BORROWED DUO" \
-  --token "TST BORROWED" \
+  --name "v1.0.3 DEFAULTED " \
+  --token "TST DEFAULTED" \
   --stable-coin-contract-address $USDC_ADDRESS_6 \
   --platform-token-contract-address $PLATFORM_TOKEN_ADDRESS \
   --min-funding-capacity 3333 \
   --max-funding-capacity 4444 \
   --funding-period-seconds 45 \
-  --lending-term-seconds 1814400 \
-  --first-loss-assets 20 \
+  --lending-term-seconds 45 \
+  --first-loss-assets 200 \
   --repayment-recurrence-days 30 \
   --grace-period-days 5 \
-  --borrower-total-interest-rate-wad 0.15 \
+  --borrower-total-interest-rate-wad 100 \
   --borrower-address $BORROWER_ADDRESS \
   --protocol-fee-wad 0.1 \
-  --default-penalty 0.1 \
+  --default-penalty 0.5 \
   --penalty-rate-wad 0.02 \
   --tranches-count 2 \
   --tranche-a-p-rs-wads "0.1,0.1" \
