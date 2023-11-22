e5393ecedf8bf8a67d32ec7e70ace18c7a155908
teddy-tribal <teddy.clapp@tribal.credit>
2023-11-22 14:21:48 -0800

    deploy pools

--- tasks/prod-encodings/encode-60-day-working-capital.sh
+++ tasks/prod-encodings/encode-60-day-working-capital.sh
@@ -6,14 +6,14 @@ export PLATFORM_TOKEN_ADDRESS="0x2B3a2582fd8a1419c394b351111a955AB6fa78D6"
 
 npx hardhat encode-pool-initializer \
   --name "60 Day Working Capital" \
-  --token "TPT" \
+  --token "TPT-2" \
   --stable-coin-contract-address $USDC_ADDRESS_6 \
   --platform-token-contract-address $PLATFORM_TOKEN_ADDRESS \
   --min-funding-capacity 80000 \
   --max-funding-capacity 100000 \
-  --funding-period-seconds 134324 \
+  --funding-period-seconds 3600 \
   --lending-term-seconds 5184000 \
-  --first-loss-assets 10000 \
+  --first-loss-assets 1500 \
   --repayment-recurrence-days 30 \
   --grace-period-days 5 \
   --borrower-total-interest-rate-wad 0.14 \
@@ -21,7 +21,7 @@ npx hardhat encode-pool-initializer \
   --protocol-fee-wad 0.1 \
   --default-penalty 0.1 \
   --penalty-rate-wad 0.02 \
-  --tranches-count 2 \
+  --tranches-count 1 \
   --tranche-a-p-rs-wads "0.14" \
   --tranche-boosted-a-p-rs-wads "0.14" \
   --tranche-boost-ratios "2" \
