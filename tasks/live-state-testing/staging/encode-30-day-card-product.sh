2c7deb1931d5363ef8f2d741bbee696d97a22df3
teddy-tribal <136001910+teddy-tribal@users.noreply.github.com>
2023-12-20 20:53:51 -0800

    add empty token

---
+++ tasks/live-state-testing/staging/encode-30-day-card-product.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export USDC_ADDRESS_6="0x07865c6E87B9F70255377e024ace6630C1Eaa37F"
+export BORROWER_ADDRESS="0x8DfA5E23c8bd7911ea7A31b180b1572B5858300B"
+export PLATFORM_TOKEN_ADDRESS="0x3b1A962BE25430793B12a359E11760c2298e325D"
+
+npx hardhat encode-pool-initializer \
+  --name "30 Day Card Product" \
+  --token "TPT-1" \
+  --stable-coin-contract-address $USDC_ADDRESS_6 \
+  --platform-token-contract-address $PLATFORM_TOKEN_ADDRESS \
+  --min-funding-capacity 40000 \
+  --max-funding-capacity 50000 \
+  --funding-period-seconds 14400 \
+  --lending-term-seconds 2592000 \
+  --first-loss-assets 500 \
+  --repayment-recurrence-days 30 \
+  --grace-period-days 5 \
+  --borrower-total-interest-rate-wad 0.1 \
+  --borrower-address $BORROWER_ADDRESS \
+  --protocol-fee-wad 0.1 \
+  --default-penalty 0.1 \
+  --penalty-rate-wad 0.02 \
+  --tranches-count 1 \
+  --tranche-a-p-rs-wads "0.1" \
+  --tranche-boosted-a-p-rs-wads "0.1" \
+  --tranche-boost-ratios "2" \
+  --tranche-coverages-wads "1" \
+  --funding-split-wads "1,1"
---
+++ tasks/live-state-testing/staging/encode-60-day-working-capital.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export USDC_ADDRESS_6="0x07865c6E87B9F70255377e024ace6630C1Eaa37F"
+export BORROWER_ADDRESS="0x8DfA5E23c8bd7911ea7A31b180b1572B5858300B"
+export PLATFORM_TOKEN_ADDRESS="0x3b1A962BE25430793B12a359E11760c2298e325D"
+
+npx hardhat encode-pool-initializer \
+  --name "60 Day Working Capital" \
+  --token "TPT-2" \
+  --stable-coin-contract-address $USDC_ADDRESS_6 \
+  --platform-token-contract-address $PLATFORM_TOKEN_ADDRESS \
+  --min-funding-capacity 80000 \
+  --max-funding-capacity 100000 \
+  --funding-period-seconds 3600 \
+  --lending-term-seconds 5184000 \
+  --first-loss-assets 1500 \
+  --repayment-recurrence-days 30 \
+  --grace-period-days 5 \
+  --borrower-total-interest-rate-wad 0.14 \
+  --borrower-address $BORROWER_ADDRESS \
+  --protocol-fee-wad 0.1 \
+  --default-penalty 0.1 \
+  --penalty-rate-wad 0.02 \
+  --tranches-count 1 \
+  --tranche-a-p-rs-wads "0.14" \
+  --tranche-boosted-a-p-rs-wads "0.14" \
+  --tranche-boost-ratios "2" \
+  --tranche-coverages-wads "1" \
+  --funding-split-wads "1,1"
