3c64fe7af7ed308cb7bb844a2a929c5c82744f12
teddy-tribal <136001910+teddy-tribal@users.noreply.github.com>
2023-12-21 17:45:37 -0800

    dev env

--- tasks/prod-init-protocol.sh
+++ tasks/prod-init-protocol.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 npx hardhat prod-init-protocol \
-  --stable-coin-address "0x07865c6E87B9F70255377e024ace6630C1Eaa37F" \
-  --fee-sharing-beneficiaries "0x92Cc142779cbA7E496cd7C724F41d6e71EffFfeF,0xCA2227dE8601baF9887083abc1FAf97558B8D382" \
+  --stable-coin-address "0xd9af0d725ABE1070e2010C421596d81810E4a633" \
+  --fee-sharing-beneficiaries "0x5aAA8d018CcFee542e4dAD3B00343bE4BeFb069E,0xCF41cd5403956BebE00EC60a3F762b10E9b08035" \
   --fee-sharing-beneficiaries-shares-wad "0.2,0.8" \
-  --owner "0xCF41cd5403956BebE00EC60a3F762b10E9b08035" \
-  --network goerli
+  --owner "0x5aAA8d018CcFee542e4dAD3B00343bE4BeFb069E" \
+  --network sepolia
 
