69b474bb441418e5a36ef0b30f50de8e12019675
teddy-tribal <136001910+teddy-tribal@users.noreply.github.com>
2024-01-17 17:33:07 -0800

    pools for rollover demo

--- tasks/live-state-testing/encodings/demotranche/encode-initial.sh
+++ tasks/live-state-testing/encodings/demotranche/encode-initial.sh
@@ -11,7 +11,7 @@ npx hardhat encode-pool-initializer \
   --platform-token-contract-address $PLATFORM_TOKEN_ADDRESS \
   --min-funding-capacity 80000 \
   --max-funding-capacity 100000 \
-  --funding-period-seconds 2592000 \
+  --funding-period-seconds 1 \
   --lending-term-seconds 31557600 \
   --first-loss-assets 5000 \
   --repayment-recurrence-days 30 \
