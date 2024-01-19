dc56308ca46ca762b2519c630faf942b0fc28fc8
teddy-tribal <136001910+teddy-tribal@users.noreply.github.com>
2024-01-19 08:47:19 -0800

    Create encode-60-day-working-capital-2.sh

---
+++ tasks/prod-encodings/encode-60-day-working-capital-2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export USDC_ADDRESS_6="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+export BORROWER_ADDRESS="0xb748289127A08AFe00948594Bf431FF138C9e9d4"
+export PLATFORM_TOKEN_ADDRESS="0x2B3a2582fd8a1419c394b351111a955AB6fa78D6"
+
+npx hardhat encode-pool-initializer \
+  --name "60 Day Working Capital #2" \
+  --token "TPT-4" \
+  --stable-coin-contract-address $USDC_ADDRESS_6 \
+  --platform-token-contract-address $PLATFORM_TOKEN_ADDRESS \
+  --min-funding-capacity 80000 \
+  --max-funding-capacity 100000 \
+  --funding-period-seconds 60 \
+  --lending-term-seconds 5184000 \
+  --first-loss-assets 1500 \
+  --repayment-recurrence-days 30 \
+  --grace-period-days 5 \
+  --borrower-total-interest-rate-wad 0.154 \
+  --borrower-address $BORROWER_ADDRESS \
+  --protocol-fee-wad 0.1 \
+  --default-penalty 0.1 \
+  --penalty-rate-wad 0.02 \
+  --tranches-count 1 \
+  --tranche-a-p-rs-wads "0.14" \
+  --tranche-boosted-a-p-rs-wads "0.14" \
+  --tranche-boost-ratios "2" \
+  --tranche-coverages-wads "1" \
+  --funding-split-wads "1,1"
