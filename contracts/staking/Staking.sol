9d7e7c8a47625055a16192a6b77d80b60f3ae0eb
Josh Ransom <josh.ransom@tribal.credit>
2024-03-21 11:59:23 +0000

    chore(imports/unused): rm

--- contracts/staking/Staking.sol
+++ contracts/staking/Staking.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "../authority/AuthorityAware.sol";
 import "./IStaking.sol";
